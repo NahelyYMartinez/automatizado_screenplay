9373211661807bc54f3abf1de0433909e927eaa5
NahelyYMartinez <113081527+NahelyYMartinez@users.noreply.github.com>
2022-11-21 10:17:31 -0400

    Add files via upload

---
+++ ui/login_ui.py
@@ -0,0 +1,13 @@
+"""
+Locators login page.
+"""
+
+
+class LoginUi:
+    base_url = "https://www.saucedemo.com/"
+    xpath_image_login = '//*[@id="root"]/div/div[2]/div[1]/div[2]'
+    input_user = 'user-name'
+    input_password = 'password'
+    button = 'login-button'
+
+
